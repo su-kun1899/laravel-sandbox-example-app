1d6410f0a616b44c4cf1fc35556c39efc0a7d0a0
su-kun1899 <higedrum.coz@gmail.com>
2022-06-18 18:18:29 +0900

    feat: :wrench: Controller の実装

--- app/Http/Controllers/Sample/IndexController.php
+++ app/Http/Controllers/Sample/IndexController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Sample;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    public function show(): string
+    {
+        return 'Hello';
+    }
 }
