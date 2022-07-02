210c3a910f0f44b5c00663ec92d7d7250ddb2c71
su-kun1899 <higedrum.coz@gmail.com>
2022-07-02 13:33:57 +0900

    feat: :wrench: つぶやきの取得処理を実装

--- app/Http/Controllers/Tweet/Update/IndexController.php
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Tweet\Update;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -16,6 +17,8 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request): Response
     {
-        //
+        $tweetId = $request->route('tweetId');
+        $tweet = Tweet::where('id', $tweetId)->firstOrFail();
+        dd($tweet);
     }
 }
