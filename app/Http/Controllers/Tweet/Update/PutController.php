faabf1d50ae5f30e6e18dbd84d5377fe7033f0b3
su-kun1899 <higedrum.coz@gmail.com>
2022-07-03 13:15:55 +0900

    feat: :wrench: 更新処理を実装

--- app/Http/Controllers/Tweet/Update/PutController.php
+++ app/Http/Controllers/Tweet/Update/PutController.php
@@ -3,19 +3,26 @@
 namespace App\Http\Controllers\Tweet\Update;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
+use App\Http\Requests\Tweet\UpdateRequest;
+use App\Models\Tweet;
+use Illuminate\Http\RedirectResponse;
 
 class PutController extends Controller
 {
     /**
      * Handle the incoming request.
      *
-     * @param Request $request
-     * @return Response
+     * @param UpdateRequest $request
+     * @return RedirectResponse
      */
-    public function __invoke(Request $request): Response
+    public function __invoke(UpdateRequest $request): RedirectResponse
     {
-        //
+        $tweet = Tweet::where('id', $request->id())->firstOrFail();
+        $tweet->content = $request->tweet();
+        $tweet->save();
+
+        return redirect()
+            ->route('tweet.update.index', ['tweetId' => $tweet->id])
+            ->with('feedback.success', 'つぶやきを編集しました');
     }
 }
