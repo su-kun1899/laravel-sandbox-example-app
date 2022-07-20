0b4719a41da8e72b8214fd7ecf40f073896f1d44
su-kun1899 <higedrum.coz@gmail.com>
2022-07-20 22:35:29 +0900

    feat: :wrench: 匿名コンポーネントを利用する

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>{{ $title ?? 'つぶやきアプリ' }}</title>
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet"/>
+    <script src="{{ mix('/js/app.js') }}"></script>
+</head>
+<body class="bg-gray-50">
+{{ $slot }}
+</body>
+</html>
