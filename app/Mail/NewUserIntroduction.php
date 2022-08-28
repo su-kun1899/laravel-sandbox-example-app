39d64ccacaff864b84e399dc7ec889576b97d4d4
su-kun1899 <higedrum.coz@gmail.com>
2022-08-28 19:35:34 +0900

    feat: :wrench: メール送信を Queue で処理する

--- app/Mail/NewUserIntroduction.php
+++ app/Mail/NewUserIntroduction.php
@@ -4,10 +4,11 @@ namespace App\Mail;
 
 use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NewUserIntroduction extends Mailable
+class NewUserIntroduction extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
