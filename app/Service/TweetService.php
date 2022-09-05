d745333200d8bb003eb883e3539ad33d577c8457
su-kun1899 <higedrum.coz@gmail.com>
2022-09-06 03:45:49 +0900

    feat: :wrench: countYesterdaysTweets を追加

--- app/Service/TweetService.php
+++ app/Service/TweetService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Models\Tweet;
+use Carbon\Carbon;
 use Illuminate\Support\Collection;
 
 class TweetService
@@ -30,5 +31,23 @@ class TweetService
         }
 
         return $tweet->user_id === $userId;
+    }
+
+    /**
+     * 前日のつぶやき数を取得する
+     *
+     * @return int
+     */
+    public function countYesterdaysTweets(): int
+    {
+        return Tweet::whereDate(
+            'created_at',
+            '>=',
+            Carbon::yesterday()->toDateTimeString()
+        )->whereDate(
+            'created_at',
+            '<',
+            Carbon::today()->toDateTimeString()
+        )->count();
     }
 }
