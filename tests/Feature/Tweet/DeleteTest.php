50e41c1d52f3f3cc7ef5daf13d85b7c6ca2fb1ff
su-kun1899 <higedrum.coz@gmail.com>
2022-09-28 23:10:33 +0900

    test: :wrench: add test_delete_success

--- tests/Feature/Tweet/DeleteTest.php
+++ tests/Feature/Tweet/DeleteTest.php
@@ -2,10 +2,15 @@
 
 namespace Tests\Feature\Tweet;
 
+use App\Models\Tweet;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class DeleteTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * A basic feature test example.
      *
@@ -13,7 +18,16 @@ class DeleteTest extends TestCase
      */
     public function test_delete_success(): void
     {
-        $response = $this->get('/tweet/delete/1');
+        // テストデータを作成
+        /** @var User $user */
+        $user = User::factory()->create();
+        /** @var Tweet $tweet */
+        $tweet = Tweet::factory()->create(['user_id' => $user->id]);
+
+        // 指定したユーザーでログインした状態にする
+        $this->actingAs($user);
+
+        $response = $this->delete('/tweet/delete/' . $tweet->id);
 
         $response->assertRedirect('/tweet');
     }
